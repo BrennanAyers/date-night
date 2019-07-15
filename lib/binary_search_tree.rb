2492a031a0e150eebb6736dbb5fe02620db90245
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-15 10:54:15 -0600

    Move all Node logic into BST object, remove Nodes, add #insert method for multiple nodes

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,18 +1,43 @@
-require './lib/node'
-
 class BinarySearchTree
-  attr_reader :head, :left, :right
+  attr_reader :key, :value, :left, :right, :depth
 
-  def initialize
-    @head = nil
+  def initialize(value = nil, key = nil, depth = 0)
+    @key = key
+    @value = value
     @left = nil
     @right = nil
+    @depth = depth
   end
 
-  def insert(value, key)
-    if @head == nil
-      @head = Node.new(value, key)
-      0
+  def insert(new_value, new_key)
+    if @value == nil && @key == nil
+      @value = new_value
+      @key = new_key
+      @depth
+    elsif new_value < @value
+      insert_left(new_value, new_key)
+    elsif new_value > @value
+      insert_right(new_value, new_key)
     end
+  end
+
+  private
+
+  def insert_left(new_value, new_key)
+    if @left
+      @left.insert(new_value, new_key, @depth + 1)
+    else
+      @left = BinarySearchTree.new(new_value, new_key, @depth + 1)
+    end
+    @left.depth
+  end
+
+  def insert_right(new_value, new_key)
+    if @right
+      @right.insert(new_value, new_key, @depth + 1)
+    else
+      @right = BinarySearchTree.new(new_value, new_key, @depth + 1)
+    end
+    @right.depth
   end
 end
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -5,9 +5,11 @@ describe BinarySearchTree do
   it 'should initialize without a key' do
     tree = BinarySearchTree.new
 
-    expect(tree.head).to be nil
+    expect(tree.value).to be nil
+    expect(tree.key).to be nil
     expect(tree.left).to be nil
     expect(tree.right).to be nil
+    expect(tree.depth).to eq(0)
   end
 
   context 'methods' do
@@ -18,14 +20,16 @@ describe BinarySearchTree do
     it 'should be able to insert a new node' do
       expect(@tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
 
-      expect(@tree.head.head).to eq({"Bill & Ted's Excellent Adventure" => 61})
+      expect(@tree.key).to eq("Bill & Ted's Excellent Adventure")
+      expect(@tree.value).to eq(61)
     end
 
     it 'should be able to insert two nodes' do
       expect(@tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
       expect(@tree.insert(16, "Johnny English")).to eq(1)
 
-      expect(@tree.left.head).to eq({"Johnny English" => 16})
+      expect(@tree.left.key).to eq("Johnny English")
+      expect(@tree.left.value).to eq(16)
     end
   end
 end
